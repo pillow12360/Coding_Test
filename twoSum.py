cb27e108d13d49ac2c23d715d8027dffab60ecf8
pillow12360 <ehdcks1256@naver.com>
2023-04-30 21:26:42 +0900

    0430 restart

---
+++ twoSum.py
@@ -0,0 +1,49 @@
+'''
+twoSum 문제
+리스트 요소 중 두개를 더해서 target 숫자가 나오면 true 아니면 false
+
+input : nums = [4,1,9,7,5,3,16] target : 14
+output : True
+
+
+일단 처음 직관적인 풀이
+2중 for 문
+'''
+#직관적 풀이
+'''
+a = [4,1,9,7,5,3,16]
+b = 14
+
+def twoSum(nums,target):
+    for i in range(len(nums)-1):
+        for j in range(i+1,len(nums)):
+            if nums[i]+nums[j]==target:
+                return True
+
+    return False
+
+print(twoSum(a,b))
+'''
+#이 코드는 O(n^2)의 시간 복잡도를 가짐
+
+#알고리즘적 풀이 twoPointer
+a = [4,1,9,7,5,3,16]
+b = 14
+
+def twoPointer(nums, target):
+    nums.sort() # O(nlogn)
+
+    left = 0
+    right = len(nums)-1
+
+    while left < right: # O(n)
+        if nums[left]+nums[right] == target:
+            return True
+        elif nums[left]+nums[right] <= target:
+            left+=1
+        elif nums[left]+nums[right] >= target:
+            right-=1
+        
+    return False
+        
+print(twoPointer(a,b))
